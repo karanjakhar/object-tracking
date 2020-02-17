2b84381d7815597e0665012e0be48ee54b6e1995
Karan Jakhar <karanjakhar49@gmail.com>
2020-02-18 00:18:11 +0530

    Add files via upload

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -1,203 +1,17 @@
 import colorsys
 import os
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
 import time
-from functools import reduce
 
-from PIL import Image
+
 import numpy as np
-from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
-import tensorflow as tf
 from keras import backend as K
 from keras.models import load_model
 from keras.layers import Input
 
-
-
-def image_preporcess(image, target_size, gt_boxes=None):
-
-    ih, iw    = target_size
-    h,  w, _  = image.shape
-
-    scale = min(iw/w, ih/h)
-    nw = int(scale * w)
-    nh = int(scale * h)
-    
-    image_resized = cv2.resize(image, (nw, nh), interpolation=cv2.INTER_CUBIC)
-
-    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
-    dw, dh = (iw - nw) // 2, (ih-nh) // 2
-    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
-
-    image_paded = np.array(image_paded, dtype='float32')
-    
-    image_paded = image_paded / 255.
-    
-    image_paded = np.expand_dims(image_paded, axis=0)
-
-    return image_paded
-
-
-def yolo_body(inputs, num_anchors, num_classes):
-    """Create YOLO_V3 model CNN body in Keras."""
-    darknet = Model(inputs, darknet_body(inputs))
-    x, y1 = make_last_layers(darknet.output, 512, num_anchors*(num_classes+5))
-
-    x = compose(
-            DarknetConv2D_BN_Leaky(256, (1,1)),
-            UpSampling2D(2))(x)
-    x = Concatenate()([x,darknet.layers[152].output])
-    x, y2 = make_last_layers(x, 256, num_anchors*(num_classes+5))
-
-    x = compose(
-            DarknetConv2D_BN_Leaky(128, (1,1)),
-            UpSampling2D(2))(x)
-    x = Concatenate()([x,darknet.layers[92].output])
-    x, y3 = make_last_layers(x, 128, num_anchors*(num_classes+5))
-
-    return Model(inputs, [y1,y2,y3])
-
-def tiny_yolo_body(inputs, num_anchors, num_classes):
-    '''Create Tiny YOLO_v3 model CNN body in keras.'''
-    x1 = compose(
-            DarknetConv2D_BN_Leaky(16, (3,3)),
-            MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'),
-            DarknetConv2D_BN_Leaky(32, (3,3)),
-            MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'),
-            DarknetConv2D_BN_Leaky(64, (3,3)),
-            MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'),
-            DarknetConv2D_BN_Leaky(128, (3,3)),
-            MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'),
-            DarknetConv2D_BN_Leaky(256, (3,3)))(inputs)
-    x2 = compose(
-            MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'),
-            DarknetConv2D_BN_Leaky(512, (3,3)),
-            MaxPooling2D(pool_size=(2,2), strides=(1,1), padding='same'),
-            DarknetConv2D_BN_Leaky(1024, (3,3)),
-            DarknetConv2D_BN_Leaky(256, (1,1)))(x1)
-    y1 = compose(
-            DarknetConv2D_BN_Leaky(512, (3,3)),
-            DarknetConv2D(num_anchors*(num_classes+5), (1,1)))(x2)
-
-    x2 = compose(
-            DarknetConv2D_BN_Leaky(128, (1,1)),
-            UpSampling2D(2))(x2)
-    y2 = compose(
-            Concatenate(),
-            DarknetConv2D_BN_Leaky(256, (3,3)),
-            DarknetConv2D(num_anchors*(num_classes+5), (1,1)))([x2,x1])
-
-    return Model(inputs, [y1,y2])
-
-
-def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
-    """Convert final layer features to bounding box parameters."""
-    num_anchors = len(anchors)
-    # Reshape to batch, height, width, num_anchors, box_params.
-    anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, num_anchors, 2])
-
-    grid_shape = K.shape(feats)[1:3] # height, width
-    grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
-        [1, grid_shape[1], 1, 1])
-    grid_x = K.tile(K.reshape(K.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]),
-        [grid_shape[0], 1, 1, 1])
-    grid = K.concatenate([grid_x, grid_y])
-    grid = K.cast(grid, K.dtype(feats))
-
-    feats = K.reshape(
-        feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
-
-    # Adjust preditions to each spatial grid point and anchor size.
-    box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[::-1], K.dtype(feats))
-    box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[::-1], K.dtype(feats))
-    box_confidence = K.sigmoid(feats[..., 4:5])
-    box_class_probs = K.sigmoid(feats[..., 5:])
-
-    if calc_loss == True:
-        return grid, feats, box_xy, box_wh
-    return box_xy, box_wh, box_confidence, box_class_probs 
-
-def yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape):
-    '''Get corrected boxes'''
-    box_yx = box_xy[..., ::-1]
-    box_hw = box_wh[..., ::-1]
-    input_shape = K.cast(input_shape, K.dtype(box_yx))
-    image_shape = K.cast(image_shape, K.dtype(box_yx))
-    new_shape = K.round(image_shape * K.min(input_shape/image_shape))
-    offset = (input_shape-new_shape)/2./input_shape
-    scale = input_shape/new_shape
-    box_yx = (box_yx - offset) * scale
-    box_hw *= scale
-
-    box_mins = box_yx - (box_hw / 2.)
-    box_maxes = box_yx + (box_hw / 2.)
-    boxes =  K.concatenate([
-        box_mins[..., 0:1],  # y_min
-        box_mins[..., 1:2],  # x_min
-        box_maxes[..., 0:1],  # y_max
-        box_maxes[..., 1:2]  # x_max
-    ])
-
-    # Scale boxes back to original image shape.
-    boxes *= K.concatenate([image_shape, image_shape])
-    return boxes 
-
-
-def yolo_boxes_and_scores(feats, anchors, num_classes, input_shape, image_shape):
-    '''Process Conv layer output'''
-    box_xy, box_wh, box_confidence, box_class_probs = yolo_head(feats,
-        anchors, num_classes, input_shape)
-    boxes = yolo_correct_boxes(box_xy, box_wh, input_shape, image_shape)
-    boxes = K.reshape(boxes, [-1, 4])
-    box_scores = box_confidence * box_class_probs
-    box_scores = K.reshape(box_scores, [-1, num_classes])
-    return boxes, box_scores
-
-def yolo_eval(yolo_outputs,
-              anchors,
-              num_classes,
-              image_shape,
-              max_boxes=20,
-              score_threshold=.6,
-              iou_threshold=.5):
-    """Evaluate YOLO model on given input and return filtered boxes."""
-    num_layers = len(yolo_outputs)
-    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]] # default setting
-    input_shape = K.shape(yolo_outputs[0])[1:3] * 32
-    boxes = []
-    box_scores = []
-    for l in range(num_layers):
-        _boxes, _box_scores = yolo_boxes_and_scores(yolo_outputs[l],
-            anchors[anchor_mask[l]], num_classes, input_shape, image_shape)
-        boxes.append(_boxes)
-        box_scores.append(_box_scores)
-    boxes = K.concatenate(boxes, axis=0)
-    box_scores = K.concatenate(box_scores, axis=0)
-
-    mask = box_scores >= score_threshold
-    max_boxes_tensor = K.constant(max_boxes, dtype='int32')
-    boxes_ = []
-    scores_ = []
-    classes_ = []
-    for c in range(num_classes):
-        # TODO: use keras backend instead of tf.
-        class_boxes = tf.boolean_mask(boxes, mask[:, c])
-        class_box_scores = tf.boolean_mask(box_scores[:, c], mask[:, c])
-        nms_index = tf.image.non_max_suppression(
-            class_boxes, class_box_scores, max_boxes_tensor, iou_threshold=iou_threshold)
-        class_boxes = K.gather(class_boxes, nms_index)
-        class_box_scores = K.gather(class_box_scores, nms_index)
-        classes = K.ones_like(class_box_scores, 'int32') * c
-        boxes_.append(class_boxes)
-        scores_.append(class_box_scores)
-        classes_.append(classes)
-    boxes_ = K.concatenate(boxes_, axis=0)
-    scores_ = K.concatenate(scores_, axis=0)
-    classes_ = K.concatenate(classes_, axis=0)
-
-    return boxes_, scores_, classes_
-
-
+from model import yolo_eval, yolo_body, tiny_yolo_body
+from utils import image_preporcess
 
 class YOLO(object):
     _defaults = {
@@ -304,33 +118,35 @@ class YOLO(object):
             box = out_boxes[i]
             score = out_scores[i]
 
-            label = '{} {:.2f}'.format(predicted_class, score)
-            #label = '{}'.format(predicted_class)
-            scores = '{:.2f}'.format(score)
+            if score > 0.6:
 
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.shape[0], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.shape[1], np.floor(right + 0.5).astype('int32'))
+	            label = '{} {:.2f}'.format(predicted_class, score)
+	            #label = '{}'.format(predicted_class)
+	            scores = '{:.2f}'.format(score)
 
-            mid_h = (bottom-top)/2+top
-            mid_v = (right-left)/2+left
+	            top, left, bottom, right = box
+	            top = max(0, np.floor(top + 0.5).astype('int32'))
+	            left = max(0, np.floor(left + 0.5).astype('int32'))
+	            bottom = min(image.shape[0], np.floor(bottom + 0.5).astype('int32'))
+	            right = min(image.shape[1], np.floor(right + 0.5).astype('int32'))
 
-            # put object rectangle
-            cv2.rectangle(image, (left, top), (right, bottom), self.colors[c], thickness)
+	            mid_h = (bottom-top)/2+top
+	            mid_v = (right-left)/2+left
 
-            # get text size
-            (test_width, text_height), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, thickness/self.text_size, 1)
+	            # put object rectangle
+	            cv2.rectangle(image, (left, top), (right, bottom), self.colors[c], thickness)
 
-            # put text rectangle
-            cv2.rectangle(image, (left, top), (left + test_width, top - text_height - baseline), self.colors[c], thickness=cv2.FILLED)
+	            # get text size
+	            (test_width, text_height), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, thickness/self.text_size, 1)
 
-            # put text above rectangle
-            cv2.putText(image, label, (left, top-2), cv2.FONT_HERSHEY_SIMPLEX, thickness/self.text_size, (0, 0, 0), 1)
+	            # put text rectangle
+	            cv2.rectangle(image, (left, top), (left + test_width, top - text_height - baseline), self.colors[c], thickness=cv2.FILLED)
 
-            # add everything to list
-            ObjectsList.append([top, left, bottom, right, mid_v, mid_h, label, scores])
+	            # put text above rectangle
+	            cv2.putText(image, label, (left, top-2), cv2.FONT_HERSHEY_SIMPLEX, thickness/self.text_size, (0, 0, 0), 1)
+
+	            # add everything to list
+	            ObjectsList.append([top, left, bottom, right, mid_v, mid_h, label, scores])
 
         return image, ObjectsList
 
@@ -345,8 +161,7 @@ class YOLO(object):
         r_image, ObjectsList = self.detect_image(original_image_color)
         return r_image, ObjectsList
 
-
-
+    
 if __name__=="__main__":
     yolo = YOLO()
 
@@ -357,23 +172,33 @@ if __name__=="__main__":
     # Set primarry FPS to 0
     fps = 0
 
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
     # we create the video capture object cap
-    cap = cv2.VideoCapture('PUT YOUR VIDEO PATH HERE')
+    cap = cv2.VideoCapture('Murder Suspect Fires On Police During High-Speed Chase - TODAY.mp4')
     if not cap.isOpened():
         raise IOError("We cannot open webcam")
 
     while True:
         ret, frame = cap.read()
+        if not ret:
+        	break
         # resize our captured frame if we need
-        frame = cv2.resize(frame, None, fx=1.0, fy=1.0, interpolation=cv2.INTER_AREA)
+        #frame = cv2.resize(frame, None, fx=1.0, fy=1.0, interpolation=cv2.INTER_AREA)
 
         # detect object on our frame
         r_image, ObjectsList = yolo.detect_img(frame)
 
         # show us frame with detection
-        cv2.imshow("Web cam input", r_image)
+        #cv2.imshow("Web cam input", r_image)
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        r_image = cv2.resize(r_image,(640,480))
+         # write the flipped frame
+        out.write(r_image)
+
         if cv2.waitKey(25) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
             break
         
         # calculate FPS
@@ -386,5 +211,4 @@ if __name__=="__main__":
 
 
     cap.release()
-    cv2.destroyAllWindows()
     yolo.close_session()
